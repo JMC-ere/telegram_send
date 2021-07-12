d91a5d9c7a232f0ae4da896f437963556bc8c7e1
김은진 <ejkim1111@3softplus.com>
2021-07-12 14:05:36 +0900

    메일 규격 변경

--- src/nudge_check.py
+++ src/nudge_check.py
@@ -3,109 +3,94 @@ from datetime import datetime, timedelta
 from elasticsearch import Elasticsearch
 from send_mail import mail_send
 
-today = datetime.today()
-yesterday = today - timedelta(days=1)
-
 
 def send_message():
+    today = datetime.today()
+    yesterday = today - timedelta(days=1)
+    yesterday = yesterday.strftime('%Y.%m.%d')
 
-    index_es = ''
-    err_cnt = 0
+    es_client = ''
 
-    try:
+    index_name = "index-nudge-complete-all"
+    yesterday_index_name = f"index-nudge-complete-all-{yesterday}"
 
-        index_es = Elasticsearch(
-            ["121.125.71.151", "121.125.71.152", "121.125.71.153", "121.125.71.154", "121.125.71.155"],
-            port=9200, max_retries=500,
-            http_auth=("elastic", "X7XsyxLzbrFDFMyOy3Wd"))
+    err_count = 0
 
-        index_name = "index-nudge-complete-all"
+    es_client = Elasticsearch(
+        ["121.125.71.151", "121.125.71.152", "121.125.71.153", "121.125.71.154", "121.125.71.155"],
+        port=9200, max_retries=500,
+        http_auth=("elastic", "X7XsyxLzbrFDFMyOy3Wd"))
 
-        body = """
-        {
-           "size": 0,
-           "query": {
-             "match_all": {}
-           },"aggs": {
-             "group_by_state": {
-               "terms": {
-                 "field": "nudge_type",
-                 "size": 10
+    body = """
+            {
+               "size": 0,
+               "query": {
+                 "match_all": {}
+               },"aggs": {
+                 "group_by_state": {
+                   "terms": {
+                     "field": "nudge_type",
+                     "size": 10
+                   }
+                 }
                }
              }
-           }
-         }
-         """
+             """
 
-        result_es = index_es.search(index=index_name, body=body)
-        result_es = result_es['aggregations']['group_by_state']['buckets']
+    message_text = '<table style="border-collapse: collapse" border="1px">'
 
-        arr_type = ['zapping', 'vod-recom', 'interest', 'prepurchase', 'dcmark', 'noresult', 'nolgs', 'active']
-        # arr_kr_type = {'zapping': 'Zapping시 컨텐츠 제안',
-        #                'vod-recom': '본방사수 실패한 컨텐츠 제안',
-        #                'interest': '관심 컨텐츠의 신규 에피소드',
-        #                'prepurchase': '구매중 이탕 컨텐츠 가격할인',
-        #                'dcmark': '찜하고 시청하지 않은 컨텐츠 가격할인',
-        #                'noresult': '검색시 미수급 컨텐츠 수급',
-        #                'nolgs': '시청 종료일이 다가오는 컨텐츠 시청제안',
-        #                'active': '관심 VOD'}
+    message_text += f"<tr><th>구분</th><th>{today.strftime('%Y.%m.%d')}</th><th>{yesterday}</th></tr>"
 
-        # type 이 색인이 되지 않으면 결과가 수집되지 않음.
-        for row in result_es:
-            print(row)
-            if row['key'] in arr_type:
-                arr_type.remove(row['key'])
+    #  Alias Check start
+    check_alias = es_client.indices.exists_alias(name=index_name)
+    if check_alias:
+        check_alias_name = es_client.indices.get_alias(name="index-nudge-complete-all")
+        key = check_alias_name.keys()
+        alias_message = "Alias Name : " + list(key)[0]
+    else:
+        alias_message = "Alias Name : Alias Error"
+    # Alias Check end
 
-        #  안정화 기간 지나면 삭제-------------------start-----------------------
-        test_list = []
-        t_cnt = 0
+    result_es = es_client.search(index=index_name, body=body)
+    yesterday_result_es = es_client.search(index=yesterday_index_name, body=body)
 
-        message_text = ''
+    result_es = result_es['aggregations']['group_by_state']['buckets']
+    yesterday_result_es = yesterday_result_es['aggregations']['group_by_state']['buckets']
 
-        check_alias = index_es.indices.exists_alias(name="index-nudge-complete-all")
+    arr_type = ['zapping', 'vod-recom', 'interest', 'prepurchase', 'dcmark', 'noresult', 'nolgs', 'active']
 
-        # 알리아스 체크------------start
-        if check_alias:
-            check_alias_name = index_es.indices.get_alias(name="index-nudge-complete-all")
-            key = check_alias_name.keys()
-            alias_message = "\nAlias Name : " + list(key)[0]
+    dict_result = {}
+    yesterday_dict_result = {}
+
+    for result in result_es:
+        if result['key'] != '':
+            dict_result[result['key']] = result['doc_count']
+
+    for result in yesterday_result_es:
+        if result['key'] != '':
+            yesterday_dict_result[result['key']] = result['doc_count']
+
+    for key_type in arr_type:
+        if key_type in dict_result:
+            message_text += f"<tr><td>{key_type}</td><td>{str(format(dict_result[key_type], ','))} 건</td>"
+            print(dict_result)
         else:
-            alias_message = "\n" + "Alias Name : Alias Error"
-        # 알리아스 체크------------end
+            print(dict_result)
+            message_text += f"<tr><td>{key_type}</td><td> Empty [*확인필요*]</td>"
+            err_count += 1
 
-        if len(arr_type) > 0:
-            for type1 in arr_type:
-                message_text += type1 + " : " + "empty [*확인필요*]<br>"
-                err_cnt += 1
+        if key_type in yesterday_dict_result:
+            message_text += f"<td>{str(format(yesterday_dict_result[key_type], ','))} 건</td></tr>"
+        else:
+            message_text += f"<td>0 건</td></tr>"
 
-        for es_row in result_es:
-            test_list.append(es_row['doc_count'])
+    message_text += f"</table><br>"
 
-            if test_list[t_cnt] != 0:
-                if es_row['key'] != "":
-                    message_text += es_row['key'] + " : " + str(format(test_list[t_cnt], ',')) + "건<br>"
+    message_text += alias_message
 
-            t_cnt = t_cnt + 1
+    h_message_text = datetime.today().strftime("%Y-%m-%d") + f" NUDGE 모니터링 결과 이슈 : {err_count}건"
 
-        test_list.clear()
-
-        message_text += alias_message
-
-        h_message_text = datetime.today().strftime("%Y-%m-%d") + f" NUDGE 모니터링 결과 이슈 : {err_cnt}건"
-
-        mail_send(h_message_text, message_text)
-
-        #  안정화 기간 지나면 삭제-------------------end-------------------------
-
-        index_es.close()
-
-    except Exception as e:
-        index_es.close()
-        h_message_text = datetime.today().strftime("%Y-%m-%d") + f" NUDGE 모니터링 결과 이슈 : {err_cnt}건"
-        message = today.strftime("%Y.%m.%d") + " NUDGE 모니터링 모듈 이슈<br>"
-        message += str(e.__cause__)
-
-        mail_send(h_message_text, message)
+    mail_send(h_message_text, message_text)
 
 
 if __name__ == '__main__':
