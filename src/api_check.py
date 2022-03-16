628032524015e52db1009fa21e4a61c3bdc9efef
JMC-ere <18118j@naver.com>
2022-03-16 15:29:32 +0900

    Update api_check.py

--- src/api_check.py
+++ src/api_check.py
@@ -5,10 +5,11 @@ from datetime import datetime
 from send_mail import mail_send
 
 
-def check_api():
+def check_api(ver):
 
     today = datetime.today().strftime("%Y.%m.%d")
 
+    result = ""
     message = ""
     server_list = ['121.125.71.171:8080', '121.125.71.172:8080', '121.125.71.173:8080',
                    '1.255.46.181:8080', '1.255.46.182:8080', '1.255.46.183:8080', '1.255.46.184:8080']
@@ -19,7 +20,10 @@ def check_api():
 
     try:
         for server in server_list:
-            url = "http://" + server + "/nudge/v1/if-nudge-001?if=IF-NUGDE-001&stb_id=%7B3E25080A-4345-11EA-819D-37B877ECC397%7D&model_group=STB&ver=5.2.4&model_name=BKO-UH400&ui_name=BKO-UH400&response_format=json&menu_id=all&menu_ids=&nudge_data"
+            if ver == "5.2.4":
+                url = "http://" + server + f"/nudge/v1/if-nudge-001?if=IF-NUGDE-001&stb_id=%7B3E25080A-4345-11EA-819D-37B877ECC397%7D&model_group=STB&ver={ver}&model_name=BKO-UH400&ui_name=BKO-UH400&response_format=json&menu_id=all&menu_ids=&nudge_data"
+            else:
+                url = "http://" + server + f"/nudge/v1/if-nudge-001?if=IF-NUGDE-001&stb_id=%7B3E25080A-4345-11EA-819D-37B877ECC397%7D&model_group=STB&ver={ver}&model_name=BKO-UH400&ui_name=BKO-UH400&response_format=json&menu_ids=menu001,menu002,menu003,menu004,menu006,menu008,menu009,menu010,menu011,menu012,menu013,menu014,menu017,menu5206,menu5205,menu5220,menu5209,menu5208,menu5212,menu5221,menu5210&nudge_data"
 
             headers = {'Content-Type': 'application/json;charset=utf-8',
                        'Accept': 'application/json;charset=utf-8',
@@ -34,6 +38,8 @@ def check_api():
             f = urllib.request.urlopen(req)
             f = f.read().decode('utf-8')
 
+            result = f
+
             f = json.loads(f)
 
             if f['result'] == '0000':
@@ -45,14 +51,21 @@ def check_api():
 
         if cnt == 7:
             message += f"\n<7개 서버 응답 정상>"
-            h_message = f"<{today} API CHECK> : [이슈 : 0]"
+            message += "<p>"
+            message += result
+            h_message = f"{ver} <{today} API CHECK> : [이슈 : 0]"
+
         else:
-            h_message = f"<{today} API CHECK> : [이슈 : {err_cnt}]"
+            h_message = f"{ver} <{today} API CHECK> : [이슈 : {err_cnt}]"
 
     except Exception as err:
-        h_message = f"<{today} API CHECK> : [이슈 : {err_cnt}]"
+        h_message = f"{ver} <{today} API CHECK> : [이슈 : {err_cnt}]"
         message += str(err)
 
+    print(message)
     mail_send(h_message, message)
 
-check_api()
+
+if __name__ == '__main__':
+    check_api("5.2.4")
+    check_api("5.3.2")
